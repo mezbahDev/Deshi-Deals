6759472e22b770ed00aed5a7314ddf84628a3d9a
mezbahDev <mezbahuddin9029@gmail.com>
2025-09-02 12:41:16 +0600

    set id and class on tag. totalPrice function updated

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
 import "./index.css";
+
+// access the cart value
+
+const totalPrice = document.getElementById("totalPrice");
+const total = document.getElementById("total");
+const discount = document.getElementById("discount");
+
+let currentTotal = 0;
+
+// access the buy button of product
+
+const buyBtns = document.querySelectorAll(".buyButton");
+const productPrice = document.querySelectorAll(".productPrice");
+
+buyBtns.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const productCard = e.target.closest(".productCard");
+    const priceElement = productCard.querySelector(".productPrice");
+    const price = parseInt(priceElement.textContent);
+
+    currentTotal += price;
+
+    totalPrice.innerHTML = currentTotal;
+  });
+});
